4d20cc3d31ec6814411f7fdfc4bd53359dd37cb9
elianCoMa <elianhcb@gmail.com>
2023-04-09 08:44:15 -0500

    Creacion de interface en el paquete Repository

---
+++ webOnpe/src/main/java/com/onpe/repository/IGrupo.java
@@ -0,0 +1,13 @@
+package com.onpe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.onpe.model.Grupo;
+
+@Repository
+public interface IGrupo extends JpaRepository<Grupo,String>{
+	@Query(value="{call usp_getGrupoVotacion (:id)}", nativeQuery = true )
+	Grupo getGrupo(String id);
+}
