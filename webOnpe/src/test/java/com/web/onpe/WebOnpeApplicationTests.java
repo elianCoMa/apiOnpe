9947559ddaf8b77cffa2b9a872d171c6b36b04c4
elianCoMa <elianhcb@gmail.com>
2023-04-09 08:29:02 -0500

    Creacion de proyecto

---
+++ webOnpe/src/test/java/com/web/onpe/WebOnpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.onpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOnpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
